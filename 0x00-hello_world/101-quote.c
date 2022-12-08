99203a4d39935ef414d9ea55bf6cdd8822033502
endalsh <ethioendu@gmail.com>
2022-12-08 10:51:07 -0800

    corraction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
-#include <unistd.h>
-/**
- * main - Prints 0 that piece of art is useful
- */
-int main(void)
-{
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
-	return (0);
-}
 
+
+/**
+ 
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ 
+ */
+
+int main(void)
+
+{
+
+   write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+
+
+ 	return (1);
+}
