836eff0cd5b6c59e723d85fb1ece396bb5cf4939
endalsh <ethioendu@gmail.com>
2022-12-08 11:51:55 -0800

    7th

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,6 +3,7 @@
 
 /**
  * main - Prints out the last part of a quote in the standard error.
+ *
  * Return: Always 1 if success.
  */
 
@@ -10,7 +11,7 @@ int main(void)
 
 {
 
-	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 61);
+	write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 59);
 
 	return (1);
 
