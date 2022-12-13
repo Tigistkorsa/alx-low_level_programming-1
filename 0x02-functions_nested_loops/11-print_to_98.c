6d841656f2b3cfd85094ac90cb4ffde3fca93385
endalsh <ethioendu@gmail.com>
2022-12-13 14:51:29 -0800

    print98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_to_98 - print to 98
+ * @n : number to start form
+ * Return:0 or 1
+ */
+
+void print_to_98(int n)
+{
+while (n < 98)
+{
+	printf("%i, ", n);
+	n--;
+}
+print("98");
+putchar('\n';
+}
