ddec9a6e6fc8d9b1dbf8976a3ff3f51939669b84
endalsh <ethioendu@gmail.com>
2022-12-13 08:01:53 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include"main.h"
+
+/*This program prints putchar
+ *
+ *entery point main
+ */
+
+int main(void)
+{
+	__putchar("-");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");
+	_putchar("a");
+	_putchar("r");
+
+	return(0);
+}
+	
+
