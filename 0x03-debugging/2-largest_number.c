c6058594ee0e25ab0bc7fc9ee9cd6603da45776d
endalsh <ethioendu@gmail.com>
2022-12-14 06:59:14 -0800

    fibonacci

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,16 +10,16 @@
 
 nt largest_number(int a, int b, int c)
 {
-	int largest = b;
+int largest = b;
 
-	if (a <= b)
-	{
-		largest = a;
-	}
-	else if (c > b)
-	{
-		largest = c;
-	}
+if (a > b)
+{
+largest = a;
+}
+else if (c > b)
+{
+largest = c;
+}
 
-	return (largest);
+return (largest);
 }
