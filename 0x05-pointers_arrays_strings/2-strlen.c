a681dcb458439a072f5bc50bee9f465e80324ba8
endalsh <ethioendu@gmail.com>
2022-12-19 09:32:36 -0800

    project

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -7,12 +7,12 @@
  */
 int _strlen(char *s)
 {
-	int len;
+int len;
 
-	while (*s != '\0')
-	{
-		len = len + 1;
-		s = s + 1;
-	}
-	return (len);
+while (*s != '\0')
+{
+len = len + 1;
+s = s + 1;
+}
+return (len);
 }
