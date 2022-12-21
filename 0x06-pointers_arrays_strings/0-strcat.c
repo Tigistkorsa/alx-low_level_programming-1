fd8ce31b4107d3904851fad3fd68011b21836068
endalsh <ethioendu@gmail.com>
2022-12-21 07:19:29 -0800

    project

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,17 +9,13 @@
 
 char *_strcat(char *dest, char *src);
 {
-char  s1[20] = "src";
-char s2[] = "dest";
-strcat(s1, s2);
-l1=strlen(s1);
-l2=strlen(s2);
-for (i=0; i<=l2; i++)
-{
-s1[l1+i]=s2[i];
-}
+int index = 0, dest_len = 0;
 
+while (dest[index++])
+dest_len++;
 
+for (index = 0; src[index]; index++)
+dest[dest_len++] = src[index];
 
 return (dest);
 }
