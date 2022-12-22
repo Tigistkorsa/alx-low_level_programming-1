95d7f772f18b65b15477c6db8975b70938f68bfd
endalsh <ethioendu@gmail.com>
2022-12-22 05:03:02 -0800

    project

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+#include "main.h"
+
 /**
 * infinite_add - C function that adds two numbers stored
 *in strings to a buffer.
@@ -26,35 +28,35 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 * @n2: The string containing the second number to be added.
 * @r: The buffer to store the result.
 * @r_index: The current index of the buffer.
+*
 * Return: If r can store the sum - a pointer to the result.
 *         If r cannot store the sum - 0.
 */
 
 char *add_strings(char *n1, char *n2, char *r, int r_index)
 {
-int num, tens = 0;
+	int num, tens = 0;
 
-for (; *n1 && *n2; n1--, n2--, r_index--)
-{
-num = (*n1 - '0') + (*n2 - '0');
-num += tens;
-*(r + r_index) = (num % 10) + '0';
-tens = num / 10;
-}
+	for (; *n1 && *n2; n1--, n2--, r_index--)
+	{
+		num = (*n1 - '0') + (*n2 - '0');
+		num += tens;
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
 
-for (; *n1; n1--; r_index++)
-{
-num = *(n1 - '0') + tens; 
-*(r + r_index) = (num % 10) + '0';
-tens = num / 10;
-}
+	for (; *n1; n1--; r_index++)
+	{
+		num = *(n1 - '0') + tens; 
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10;
+	}
 
-for (; *n2; n2--;  r_index--)
-{
-num = (*n2 - '0') + tens; 
-*(r + r_index) = (num % 10) + '0';
-tens = num / 10; 
-}
+	for (; *n2; n2--;  r_index--)
+	{
+		num = (*n2 - '0') + tens; 
+		*(r + r_index) = (num % 10) + '0';
+		tens = num / 10; 
+	}
 	
 }
-
