bfab0013a0f004883f25222c35ad68897f6f0f10
endalsh <ethioendu@gmail.com>
2023-01-04 01:57:38 -0800

    project

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _put_recursion - print a string
+ * @s: string
+ * Return: void
+ */
+void _put_recursion(char *s)
+{
+_putchar(*s);
+_put_recursion(s + 1);
+}
+else
+{
+_putchar('\n');
+}
+}
