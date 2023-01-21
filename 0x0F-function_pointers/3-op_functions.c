918c957ece4c733087868d85273648e460874f27
FIRST_NAME LAST_NAME <ethioendu21@gmail.com>
2023-01-21 03:05:41 -0800

    project

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,53 +1,75 @@
 #include "3-calc.h"
 #include <stdlib.h>
 #include <stdio.h>
+
 /**
- * op_add - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
- */
+  * op_add - function with two arguments
+  * @a: int type
+  * @b: int type
+  *
+  * Return: value
+  */
 int op_add(int a, int b)
 {
 	return (a + b);
 }
+
 /**
- * op_sub - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
- */
+  * op_sub - function with two arguments
+  * @a: int type
+  * @b: int type
+  *
+  * Return: value
+  */
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
+
 /**
- * op_mul - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
- */
+  * op_mul - function with two arguments
+  * @a: int type
+  * @b: int type
+  *
+  * Return: value
+  */
 int op_mul(int a, int b)
 {
 	return (a * b);
 }
+
 /**
- * op_div - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
- */
+  * op_div - function with two arguments
+  * @a: int type
+  * @b: int type
+  *
+  * Return: value
+  */
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
 	return (a / b);
 }
+
 /**
- * op_mod - 5 functions
- * @a: input one
- * @b: input two
- * Return: 0
- */
+  * op_mod - function with two arguments
+  * @a: int type
+  * @b: int type
+  *
+  * Return: valuew
+  */
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
 	return (a % b);
 }
