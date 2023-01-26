31f7c82b2d411fae04fa7a1313db8633338d4e66
FIRST_NAME LAST_NAME <ethioendu21@gmail.com>
2023-01-26 08:58:51 -0800

    project

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,11 +8,20 @@
   */
 size_t print_list(const list_t *h)
 {
-size_t i;
-for(i = 0; h; i++)
-{
-printf("[%d] %s \n", h->len, h->str);
-h = h->next;
-}
-return(i);
+int count = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+		printf("[%d] %s\n", h->len, h->str);
+		}
+		count++;
+		h = h->next;
+	}
+	return (count);
 }
